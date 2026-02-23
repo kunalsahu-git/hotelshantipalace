2c4355e83988062701ac35a0f6c378539ecbe31b
Kunal Sahu <kunal.sahu@acquia.com>
2026-02-23 17:34:38 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // NOTE: This is a placeholder configuration.
 // Replace it with your project's actual Firebase configuration.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD4-gmDK2t2oEI9EgY7ksUYh-O2RB45_kU",
+  authDomain: "studio-3769306278-2cbbf.firebaseapp.com",
+  projectId: "studio-3769306278-2cbbf",
+  storageBucket: "studio-3769306278-2cbbf.appspot.com",
+  messagingSenderId: "562249108492",
+  appId: "1:562249108492:web:81e57976b199120595f229"
 };
