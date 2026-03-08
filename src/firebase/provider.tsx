05ae0ecd053b0a42c50b32adc194ccbc71a0fd37
kunalsahubb <kunalsahubb@gmail.com>
2026-03-08 10:32:08 +0000

    Storage

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -4,7 +4,7 @@ import React, { DependencyList, createContext, useContext, ReactNode, useMemo, u
 import { FirebaseApp } from 'firebase/app';
 import { Firestore } from 'firebase/firestore';
 import { Auth, User, onAuthStateChanged } from 'firebase/auth';
-import type { Storage } from 'firebase/storage';
+import type { FirebaseStorage as Storage } from 'firebase/storage';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener'
 
 interface FirebaseProviderProps {
